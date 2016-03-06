08c6cfd92786ae5074a1909f9d6bf5b5ca0a19af
Greeng10nt <mathieuweill@yahoo.fr>
2016-03-06 18:52:42 +0100

    Update server.R

--- server.R
+++ server.R
@@ -11,8 +11,8 @@ shinyServer(function(input, output) {
   output$USPersonalExpenditureplot <- renderPlot({
     
     # Render a barplot
-    barplot(USPersonalExpenditure[,input$area]*1000, 
-            main=input$area,
+    barplot(USPersonalExpenditure[,input$en_US]*1000, 
+            main=input$en_US,
             ylab="en_US",
             xlab="Year")
   })
